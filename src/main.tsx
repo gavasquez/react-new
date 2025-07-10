d166c4c258b1dd50f7da999334dff61014117133
gavasquez <gustavovasquez15@gmail.com>
2025-07-10 17:03:51 -0500

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { TrafficLight } from './01-useState/TrafficLigth';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <TrafficLight />
+  </StrictMode>,
+)
